7a405a09d4ac3626b4b1f0e23f5df9923700b9a9
Hereshi <127274056+henrypro32@users.noreply.github.com>
2024-10-13 14:40:56 -0500

    verificaciones secante

--- backend/controllers/secante_controller.py
+++ backend/controllers/secante_controller.py
@@ -4,6 +4,26 @@ from .validation import validate_function, validate_numeric, validate_tolerance,
 
 def secante_r():
     data = request.json
+
+    valid, error_message = validate_function(data['function'])
+    if not valid:
+        return jsonify({"error": error_message}), 400
+    
+    valid, error_message = validate_numeric(data['initial_point_a'])
+    if not valid:
+        return jsonify({"error": f"initial point a: {error_message}"}), 400
+    
+    valid, error_message = validate_numeric(data['initial_point_b'])
+    if not valid:
+        return jsonify({"error": f"initial point b: {error_message}"}), 400
+    
+    valid, error_message = validate_tolerance(data['tolerance'])
+    if not valid:
+        return jsonify({"error": error_message}), 400
+    
+
  
     result = secante(data)
     return jsonify(result)
+
+    
