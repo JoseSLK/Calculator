1890187ccdb5aedd51acd123b6052a6513f560cc
JoseSLK <jose.salamanca05@uptc.edu.co>
2024-10-22 08:46:04 -0500

    [Fix] Biseccion

--- backend/methods/biseccion.py
+++ backend/methods/biseccion.py
@@ -11,18 +11,40 @@ def biseccion(data):
 
     i = 1
 
+    iteraciones = []
+
     def f(x):
         return eval(f_str)
+    
+    if f(a) * f(b) > 0:
+        return {
+            "resultado": "Error",
+            "mensaje": "No hay cambio de signo en los límites, f(a)*f(b) > 0."
+        }
 
     while i <= n:
         p = a + (b - a)/2
         #print("i = {0:<2}, p = {1:.12f}".format(i,p))
-        if abs(f(p)) <= 1e-15 or (b - a)/2 < tol:
-            return p
+        iteraciones.append({
+            "iteracion": i + 1,
+            "x": p,
+            "error" : (b - a)/2
+
+        })
+        if (b - a)/2 < tol:
+            return {
+                "resultado" : "siuuuu",
+                "x" : p,
+                "iteraciones" : iteraciones,
+            }
         i += 1
         if f(a)*f(p) > 0:
             a = p
         else:
             b = p
-    return None
+    return {
+        "resultado": "hola khe ace",
+        "iteraciones": iteraciones,
+        "mensaje": "Iteraciones agotadas sin convergencia"
+    }
     
